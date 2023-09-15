b831af2d5626abab3edba810d92da0bb03815097
level0rd <45522296+level0rd@users.noreply.github.com>
2023-09-15 21:16:54 +0300

    Optimized version

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import cv2
-import time
 import numpy as np
+import av
+
+import streamlit as st
+from streamlit_webrtc import WebRtcMode, RTCConfiguration, webrtc_streamer
 
 from face_detectors import Ultralight320Detector
 import mediapipe as mp
@@ -8,17 +11,15 @@ import mediapipe as mp
 FONT = cv2.FONT_HERSHEY_PLAIN
 LEFT_EYE_POINTS = [36, 37, 38, 39, 40, 41]
 RIGHT_EYE_POINTS = [42, 43, 44, 45, 46, 47]
-BLUE = (255, 0, 0)
+BLUE = (0, 0, 255)
 WHITE = (255, 255, 255)
-
-vid = cv2.VideoCapture("Good_no_voice.mp4")
 
 pose = mp.solutions.pose.Pose(model_complexity=0)
 detector = Ultralight320Detector()
 
+RTC_CONFIGURATION = RTCConfiguration({"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]})
 
-def midpoint(p1, p2):
-    return int((p1.x + p2.x)/2), int((p1.y + p2.y)/2)
+streaming_placeholder = st.empty()
 
 
 def det_gaze_ratio(eye_points, facial_landmarks, height, width, gray):
@@ -59,7 +60,7 @@ def det_gaze_ratio(eye_points, facial_landmarks, height, width, gray):
     return gaze_ration
 
 
-def get_gaze_ratio(img, img_height, img_width):
+def get_gaze_direction(img, img_height, img_width):
 
     gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
     bbox = detector.detect_faces_keypoints(gray, get_all=True)
@@ -79,81 +80,78 @@ def get_gaze_ratio(img, img_height, img_width):
     return gaze_direction
 
 
-pTime = 0
+def get_coordinates(number, body, img_height, img_width):
+    x = int(body.pose_landmarks.landmark[number].x * img_width)
+    y = int(body.pose_landmarks.landmark[number].y * img_height)
 
-while True:
-    rect, frame = vid.read()
-    if not rect:
-        break
+    return x, y
 
-    cTime = time.time()
 
-    image_height, image_width, _ = frame.shape
+def callback(frame: av.VideoFrame) -> av.VideoFrame:
 
-    cv2.putText(frame, get_gaze_ratio(frame,  image_height, image_width), (50, 100), FONT, 2, (255, 0, 0), 3)
+    img_rgb = frame.to_ndarray(format="rgb24")
 
-    body = pose.process(frame)
+    image_height, image_width, _ = img_rgb.shape
+
+    cv2.putText(img_rgb, get_gaze_direction(img_rgb,  image_height, image_width), (50, 100), FONT, 2, (255, 0, 0), 3)
+
+    body = pose.process(img_rgb)
 
     if body.pose_landmarks:
-        x11 = int(body.pose_landmarks.landmark[11].x * image_width)
-        y11 = int(body.pose_landmarks.landmark[11].y * image_height)
-        cv2.circle(frame, (x11, y11), 3, BLUE, 2)
-        x12 = int(body.pose_landmarks.landmark[12].x * image_width)
-        y12 = int(body.pose_landmarks.landmark[12].y * image_height)
-        cv2.circle(frame, (x12, y12), 3, BLUE, 2)
 
-        x13 = int(body.pose_landmarks.landmark[13].x * image_width)
-        y13 = int(body.pose_landmarks.landmark[13].y * image_height)
-        cv2.circle(frame, (x13, y13), 3, BLUE, 2)
-        x15 = int(body.pose_landmarks.landmark[15].x * image_width)
-        y15 = int(body.pose_landmarks.landmark[15].y * image_height)
-        cv2.circle(frame, (x15, y15), 3, BLUE, 2)
-        x17 = int(body.pose_landmarks.landmark[17].x * image_width)
-        y17 = int(body.pose_landmarks.landmark[17].y * image_height)
-        cv2.circle(frame, (x17, y17), 3, BLUE, 2)
-        x19 = int(body.pose_landmarks.landmark[19].x * image_width)
-        y19 = int(body.pose_landmarks.landmark[19].y * image_height)
-        cv2.circle(frame, (x19, y19), 3, BLUE, 2)
-        x21 = int(body.pose_landmarks.landmark[21].x * image_width)
-        y21 = int(body.pose_landmarks.landmark[21].y * image_height)
-        cv2.circle(frame, (x21, y21), 3, BLUE, 2)
+        # shoulders
+        p11 = get_coordinates(11, body, image_height, image_width)
+        cv2.circle(img_rgb, p11, 3, BLUE, 2)
+        p12 = get_coordinates(12, body, image_height, image_width)
+        cv2.circle(img_rgb, p12, 3, BLUE, 2)
 
-        x14 = int(body.pose_landmarks.landmark[14].x * image_width)
-        y14 = int(body.pose_landmarks.landmark[14].y * image_height)
-        cv2.circle(frame, (x14, y14), 3, BLUE, 2)
-        x16 = int(body.pose_landmarks.landmark[16].x * image_width)
-        y16 = int(body.pose_landmarks.landmark[16].y * image_height)
-        cv2.circle(frame, (x16, y16), 3, BLUE, 2)
-        x18 = int(body.pose_landmarks.landmark[18].x * image_width)
-        y18 = int(body.pose_landmarks.landmark[18].y * image_height)
-        cv2.circle(frame, (x18, y18), 3, BLUE, 2)
-        x20 = int(body.pose_landmarks.landmark[20].x * image_width)
-        y20 = int(body.pose_landmarks.landmark[20].y * image_height)
-        cv2.circle(frame, (x20, y20), 3, BLUE, 2)
-        x22 = int(body.pose_landmarks.landmark[22].x * image_width)
-        y22 = int(body.pose_landmarks.landmark[22].y * image_height)
-        cv2.circle(frame, (x22, y22), 3, BLUE, 2)
+        cv2.line(img_rgb, p12, p11, WHITE, 1)
 
-        cv2.line(frame, (x12, y12), (x11, y11), WHITE, 1)
+        # right hand
+        p13 = get_coordinates(13, body, image_height, image_width)
+        cv2.circle(img_rgb, p13, 3, BLUE, 2)
+        p15 = get_coordinates(15, body, image_height, image_width)
+        cv2.circle(img_rgb, p15, 3, BLUE, 2)
+        p17 = get_coordinates(17, body, image_height, image_width)
+        cv2.circle(img_rgb, p17, 3, BLUE, 2)
+        p19 = get_coordinates(19, body, image_height, image_width)
+        cv2.circle(img_rgb, p19, 3, BLUE, 2)
+        p21 = get_coordinates(21, body, image_height, image_width)
+        cv2.circle(img_rgb, p21, 3, BLUE, 2)
 
-        cv2.line(frame, (x11, y11), (x13, y13), WHITE, 1)
-        cv2.line(frame, (x13, y13), (x15, y15), WHITE, 1)
-        cv2.line(frame, (x15, y15), (x21, y21), WHITE, 1)
-        cv2.line(frame, (x15, y15), (x17, y17), WHITE, 1)
-        cv2.line(frame, (x15, y15), (x19, y19), WHITE, 1)
+        cv2.line(img_rgb, p11, p13, WHITE, 1)
+        cv2.line(img_rgb, p13, p15, WHITE, 1)
+        cv2.line(img_rgb, p15, p21, WHITE, 1)
+        cv2.line(img_rgb, p15, p17, WHITE, 1)
+        cv2.line(img_rgb, p15, p19, WHITE, 1)
 
-        cv2.line(frame, (x12, y12), (x14, y14), WHITE, 1)
-        cv2.line(frame, (x14, y14), (x16, y16), WHITE, 1)
-        cv2.line(frame, (x16, y16), (x22, y22), WHITE, 1)
-        cv2.line(frame, (x16, y16), (x20, y20), WHITE, 1)
-        cv2.line(frame, (x16, y16), (x18, y18), WHITE, 1)
+        # left hand
+        p14 = get_coordinates(14, body, image_height, image_width)
+        cv2.circle(img_rgb, p14, 3, BLUE, 2)
+        p16 = get_coordinates(16, body, image_height, image_width)
+        cv2.circle(img_rgb, p16, 3, BLUE, 2)
+        p18 = get_coordinates(18, body, image_height, image_width)
+        cv2.circle(img_rgb, p18, 3, BLUE, 2)
+        p20 = get_coordinates(20, body, image_height, image_width)
+        cv2.circle(img_rgb, p20, 3, BLUE, 2)
+        p22 = get_coordinates(22, body, image_height, image_width)
+        cv2.circle(img_rgb, p22, 3, BLUE, 2)
 
-    fps = 1 / (cTime - pTime)
-    pTime = cTime
+        cv2.line(img_rgb, p12, p14, WHITE, 1)
+        cv2.line(img_rgb, p14, p16, WHITE, 1)
+        cv2.line(img_rgb, p16, p22, WHITE, 1)
+        cv2.line(img_rgb, p16, p20, WHITE, 1)
+        cv2.line(img_rgb, p16, p18, WHITE, 1)
 
-    cv2.putText(frame, str(int(fps)), (70, 50), FONT, 3, BLUE, 3)
+    return av.VideoFrame.from_ndarray(img_rgb, format="rgb24")
 
-    cv2.imshow("Pose detection and gaze direction", frame)
 
-    cv2.waitKey(1)
-
+with streaming_placeholder.container():
+    webrtc_ctx = webrtc_streamer(
+        key="object-detection",
+        mode=WebRtcMode.SENDRECV,
+        rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]},
+        video_frame_callback=callback,
+        media_stream_constraints={"video": True, "audio": False},
+        async_processing=True,
+    )
