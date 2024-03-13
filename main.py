df6068be61db7a065a14b992969511d6e45c1a0a
level0rd <45522296+level0rd@users.noreply.github.com>
2024-03-13 16:51:57 +0300

    Update main.py
    Update the pose detection functionality by replacing the mppose model with MoveNet, improving accuracy and performance.

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 import cv2
 import numpy as np
+import math
 import av
 import time
-import copy
 from enum import Enum
-from typing import List, Tuple
+from typing import Tuple
 
 import streamlit as st
-from streamlit_webrtc import AudioProcessorBase, WebRtcMode, RTCConfiguration, webrtc_streamer
+from streamlit_webrtc import WebRtcMode, RTCConfiguration, webrtc_streamer
 
 from face_detectors import Ultralight320Detector
-import mediapipe as mp
+from openvino.runtime import Core
+import tensorflow as tf
 
 import pvcobra
 from pvrecorder import PvRecorder
+
 
 class GazeDirection(Enum):
     LEFT = "LEFT"
@@ -25,42 +27,89 @@ class HandsPosition(Enum):
     BOTTOM = "BOTTOM"
     TOP = "TOP"
 
-cobra = pvcobra.create(access_key='your_access_key')
-recorder = PvRecorder(frame_length=512, device_index=3)
+
+POSE_MODEL = 'models/move_net_openvino.xml'
+INPUT_SIZE = 256
 
 FONT = cv2.FONT_HERSHEY_PLAIN
+FONT_SIZE = 2
+FONT_THICKNESS = 3
+
 LEFT_EYE_POINTS = [36, 37, 38, 39, 40, 41]
 RIGHT_EYE_POINTS = [42, 43, 44, 45, 46, 47]
 BLUE = (0, 0, 255)
 RED = (255, 0, 0)
 WHITE = (255, 255, 255)
-FONT_SIZE = 2
-FONT_THICKNESS = 3
-CIRCLE_SIZE = 3
-LINE_THICKNESS = 1
 
-pose = mp.solutions.pose.Pose(model_complexity=0)
-detector = Ultralight320Detector()
+LEFT_GAZE_THRESHOLD = 0.85
+RIGHT_GAZE_THRESHOLD = 1.6
+MOVE_NET_THRESHOLD = 0.1
+
+cobra = pvcobra.create(access_key='your_access_key')
+recorder = PvRecorder(frame_length=512, device_index=3)
+
+core = Core()
+ov_model = core.read_model(POSE_MODEL)
+pose_model = core.compile_model(ov_model)
+
+face_detector = Ultralight320Detector()
+
 
 RTC_CONFIGURATION = RTCConfiguration({"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]})
-
 streaming_placeholder = st.empty()
 
 
+def keep_aspect_ratio_resizer(image: tf.Tensor, target_size: int) -> Tuple[tf.Tensor, Tuple[int, int]]:
+    """Resizes the image.
+
+    The function resizes the image such that its longer side matches the required
+    target_size while keeping the image aspect ratio.
+
+    Args:
+        image (tf.Tensor): Input image tensor.
+        target_size (int): Target size for the longer side of the image.
+
+    Returns:
+        tf.Tensor: Resized and padded image tensor.
+        Tuple[int, int]: Tuple containing the target height and width of the image.
+    """
+    _, height, width, _ = image.shape
+    if height > width:
+        scale = float(target_size / height)
+        target_height = target_size
+        scaled_width = math.ceil(width * scale)
+        image = tf.image.resize(image, [target_height, scaled_width])
+        target_width = round(math.ceil(scaled_width / 32) * 32)
+    else:
+        scale = float(target_size / width)
+        target_width = target_size
+        scaled_height = math.ceil(height * scale)
+        image = tf.image.resize(image, [scaled_height, target_width])
+        target_height = round(math.ceil(scaled_height / 32) * 32)
+    image = tf.image.pad_to_bounding_box(image, 0, 0, target_height, target_width)
+    return image,  (target_height, target_width)
+
+
 def get_gaze_ratio(
-	eye_points: [int], 
-	facial_landmarks: np.ndarray, 
-	height: int, 
-	width: int, 
-	gray: np.ndarray
+	eye_points: list[int],
+        facial_landmarks: np.ndarray,
+        height: int,
+        width: int,
+        gray: np.ndarray
 ) -> float:
-   	
+"""
+    Calculate the ratio of white pixels in eye regions.
+
+    Args:
+        eye_points (list[int]): List of indices representing eye points.
+        facial_landmarks (np.ndarray): Array of facial landmarks.
+        height (int): Height of the image.
+        width (int): Width of the image.
+        gray (np.ndarray): Grayscale image.
+
+    Returns:
+        float: The gaze ratio calculated based on the white pixel ratio.
     """
-
-    Calculate white pixel ratio.
-
-    """
-
     eye_region = np.array([(facial_landmarks[eye_points[0]][0], facial_landmarks[eye_points[0]][1]),
                            (facial_landmarks[eye_points[1]][0], facial_landmarks[eye_points[1]][1]),
                            (facial_landmarks[eye_points[2]][0], facial_landmarks[eye_points[2]][1]),
@@ -82,10 +131,10 @@ def get_gaze_ratio(
     _, threshold_eye = cv2.threshold(gray_eye, 70, 255, cv2.THRESH_BINARY_INV)
     height, width = threshold_eye.shape
 
-    left_side_threshold = threshold_eye[0: height, 0: int(width / 2)]
+    left_side_threshold = threshold_eye[0: height, 0: round(width / 2)]
     left_side_white = cv2.countNonZero(left_side_threshold)
 
-    right_side_threshold = threshold_eye[0: height, int(width / 2): width]
+    right_side_threshold = threshold_eye[0: height, round(width / 2): width]
     right_side_white = cv2.countNonZero(right_side_threshold)
 
     if left_side_white == 0:
@@ -100,23 +149,28 @@ def get_gaze_ratio(
 
 def get_gaze_direction(img: np.ndarray, img_height: int, img_width: int) -> str:
     """
+    Calculate the direction of the eye (left, center, right).
 
-    Calculate the direction of the eye(left, center, right).
+    Args:
+        img (np.ndarray): Input image.
+        img_height (int): Height of the image.
+        img_width (int): Width of the image.
 
+    Returns:
+        str: The direction of the gaze.
     """
-
     gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-    bbox = detector.detect_faces_keypoints(gray, get_all=True)
+    face_keypoints = face_detector.detect_faces_keypoints(gray, get_all=True)
 
-    landmarks = bbox[0]['keypoints']
+    landmarks = face_keypoints[0]['keypoints']
 
     gaze_ration_left_eye = get_gaze_ratio(LEFT_EYE_POINTS, landmarks, img_height, img_width, gray)
     gaze_ration_right_eye = get_gaze_ratio(RIGHT_EYE_POINTS, landmarks, img_height, img_width, gray)
     gaze_ratio = (gaze_ration_right_eye + gaze_ration_left_eye) / 2
 
-    if gaze_ratio <= 0.85:
+    if gaze_ratio <= LEFT_GAZE_THRESHOLD:
         gaze_direction = GazeDirection.LEFT.value
-    elif 0.85 < gaze_ratio < 1.6:
+    elif LEFT_GAZE_THRESHOLD < gaze_ratio < RIGHT_GAZE_THRESHOLD:
         gaze_direction = GazeDirection.CENTER.value
     else:
         gaze_direction = GazeDirection.RIGHT.value
@@ -124,134 +178,97 @@ def get_gaze_direction(img: np.ndarray, img_height: int, img_width: int) -> str:
     return gaze_direction
 
 
-def get_coordinates(number: int, body: mp.solutions.pose.Pose, img_height: int, img_width: int) -> Tuple[int, int]:
-    
+def second_person_detection(keypoints: np.ndarray) -> bool:
     """
+    Detect the presence of a second person in the keypoints.
 
-    Calculate the coordinates of a key point in an image.
+    Args:
+        keypoints (np.ndarray): Array of keypoints.
 
+    Returns:
+        bool: True if a second person is detected, False otherwise.
     """
+    confidences_per_instance = keypoints[0, :, -1]
+    second_person = True if confidences_per_instance[1] >= MOVE_NET_THRESHOLD else False
 
-    x = int(body.pose_landmarks.landmark[number].x * img_width)
-    y = int(body.pose_landmarks.landmark[number].y * img_height)
-
-    return x, y
+    return second_person
 
 
-def hand_filling(hand_coordinates: np.ndarray, image: np.ndarray) -> None:
+def get_movenet_keypoints(image_rgb: np.ndarray) -> np.ndarray:
     """
+     Process an input image using the MoveNet network to detect human poses.
 
-    Fill hands with color to prevent mpPose from finding them again.
+    Args:
+        image_rgb (np.ndarray): RGB image array.
 
+    Returns:
+        np.ndarray: Array containing MoveNet keypoints.
     """
-	
-    hand_coordinates = hand_coordinates.reshape((-1, 1, 2))
-    cv2.fillPoly(image, [hand_coordinates], color=WHITE)
+    _, image_encoded = cv2.imencode('.jpg', image_rgb)
+    image_bytes = image_encoded.tobytes()
+    image = tf.compat.v1.image.decode_jpeg(image_bytes)
+    image = tf.expand_dims(image, axis=0)
+
+    resized_image, image_shape = keep_aspect_ratio_resizer(image, INPUT_SIZE)
+    input_tensor = tf.cast(resized_image, dtype=tf.uint8)
+
+    return pose_model(input_tensor)[0]
 
 
 def callback(frame: av.VideoFrame) -> av.VideoFrame:
+    """
+    Process each frame of the video stream.
 
-    img_rgb = frame.to_ndarray(format="rgb24")
+    Args:
+        frame (av.VideoFrame): Input video frame.
 
-    image_height, image_width, _ = img_rgb.shape
+    Returns:
+        av.VideoFrame: Processed video frame.
+    """
+    image_rgb = frame.to_ndarray(format="rgb24")
+    image_height, image_width, _ = image_rgb.shape
+
+    keypoints = get_movenet_keypoints(image_rgb)
+
+    if second_person_detection(keypoints):
+        cv2.putText(image_rgb, 'Second person detected!', (10, 260), FONT, FONT_SIZE, RED,  FONT_THICKNESS)
+
+    p10 = (round(keypoints[0][0][28] * image_width), round(keypoints[0][0][27] * image_height))
+    p11 = (round(keypoints[0][0][31] * image_width), round(keypoints[0][0][30] * image_height))
+    hand_border = (image_height / 100) * 85
+
+    if p11[1] > hand_border or p10[1] > hand_border:
+        hands_positions = HandsPosition.BOTTOM.value
+    else:
+        hands_positions = HandsPosition.TOP.value
+    cv2.putText(image_rgb, 'Hands: ' + hands_positions, (10, 90), FONT, FONT_SIZE, RED, FONT_THICKNESS)
 
     try:
-        cv2.putText(img_rgb, 'Eyes: ' + get_gaze_direction(img_rgb, image_height, image_width), (10, 120), FONT, FONT_SIZE, 
-                   RED, FONT_THICKNESS)
+        cv2.putText(image_rgb, 'Eyes: ' + get_gaze_direction(image_rgb, image_height, image_width), (10, 120), FONT,
+                    FONT_SIZE, RED, FONT_THICKNESS)
     except IndexError:
-        cv2.putText(img_rgb, 'The examinee is absent!', (10, 200), cv2.FONT_HERSHEY_PLAIN, 3, RED, FONT_THICKNESS)
+        cv2.putText(image_rgb, 'The examinee is absent!', (10, 200), FONT, FONT_SIZE + 1, RED, FONT_THICKNESS)
+        print("Ошибка: индекс списка вне диапазона")
 
-    body = pose.process(img_rgb)
-
-    img_filled_person = copy.deepcopy(img_rgb)
-    point_color = BLUE
-
-    if body.pose_landmarks:
-	
-	#################
-        # Drawing hands #
-        #################
-	    
-	#  of landmark pairs forming hands
-    hand_landmarks = [
-        [11, 12, 13, 15, 21],  # Right hand
-        [12, 14, 16, 22, 18]   # Left hand
-    ]
-
-    #  of landmark pairs forming shoulders
-    shoulder_landmarks = [[11, 12], [1, 2], [1, 11], [2, 12]]
-
-    for landmarks in hand_landmarks:
-        for i in range(len(landmarks) - 1):
-            p1 = get_coordinates(landmarks[i], body, image_height, image_width)
-            p2 = get_coordinates(landmarks[i + 1], body, image_height, image_width)
-            cv2.line(img_rgb, p1, p2, WHITE, LINE_THICKNESS)
-            cv2.circle(img_rgb, p1, CIRCLE_SIZE, point_color, -1)
-            if i == len(landmarks) - 2:  # For the last point
-                cv2.circle(img_rgb, p2, CIRCLE_SIZE, point_color, -1)
-
-    for landmarks in shoulder_landmarks:
-        p1 = get_coordinates(landmarks[0], body, image_height, image_width)
-        p2 = get_coordinates(landmarks[1], body, image_height, image_width)
-        cv2.line(img_rgb, p1, p2, WHITE, LINE_THICKNESS)
-        cv2.circle(img_rgb, p1, CIRCLE_SIZE, point_color, -1)
-        cv2.circle(img_rgb, p2, CIRCLE_SIZE, point_color, -1)
-
-        bbox_cor_x = np.array([p1[0], p2[0], p3[0], p4[0], p5[0], p6[0], p7[0], p8[0], p12[0], p13[0], p14[0]])
-        bbox_cor_y = np.array([p1[1], p2[1], p3[1], p4[1], p5[1], p6[1], p7[1], p8[1], p12[1], p13[1], p14[1]])
-
-        # Right hand polygon
-        right_hand_points = np.array([[p20[0], p20[1] - 10], [p22[0], p22[1]], [p14[0], p14[1] - 10], [p14[0], p14[1] + 10], [p18[0], p18[1] + 10]], np.int32)
-        hand_filling(right_hand_points, img_croped)
-
-        # Left hand polygon
-        reft_hand_points = np.array([[p13[0], p13[1] - 10], [p13[0], p13[1] + 10], [p21[0], p21[1]], [p19[0], p19[1] - 10], [p17[0], p17[1] + 10]], np.int32)
-        hand_filling(reft_hand_points, img_croped)
-
-        bb_y_min = np.min(bbox_cor_y)
-        bb_y_max = np.max(bbox_cor_y)
-
-        bb_x_min = np.min(bbox_cor_x)
-        bb_x_max = np.max(bbox_cor_x)
-
-        # Expand bbox
-        if image_height - bb_y_max <= 35:
-            bb_y_max = image_height
-
-        if image_width - bb_x_max <= 35:
-            bb_x_max = image_width
-
-        # Draw filled bbox
-        cv2.rectangle(img_filled_person, (bb_x_min, bb_y_min-70), (bb_x_max, bb_y_max), WHITE, -1)
-
-        body_second = pose.process(img_filled_person)
-
-        if body_second.pose_landmarks:
-            cv2.putText(img_filled_person, 'Second person detected!', (10, 260), cv2.FONT_HERSHEY_PLAIN, FONT_SIZE, RED, FONT_THICKNESS)
-
-
-        # Hands position detection
-        hand_border = (image_height/100)*88
-
-        if (p16[1] > hand_border and p18[1] > hand_border and p20[1] > hand_border and p22[1] > hand_border) or \
-                (p15[1] > hand_border and p17[1] > hand_border and p19[1] > hand_border and p21[1] > hand_border):
-            hands_positions = HandsPosition.BOTTOM.value
-        else:
-            hands_positions = HandsPosition.TOP.value
-
-        cv2.putText(img_rgb, 'Hands: ' + hands_positions, (10, 90), cv2.FONT_HERSHEY_PLAIN, FONT_SIZE, RED, FONT_THICKNESS)
-
-    return av.VideoFrame.from_ndarray(img_rgb, format="rgb24")
+    return av.VideoFrame.from_ndarray(image_rgb, format="rgb24")
 
 
 def process_audio(frame: av.AudioFrame) -> av.AudioFrame:
+    """
+    Detect voice presence in each audio frame of the stream using Cobra Voice Activity Detection.
 
+    Args:
+        frame (av.AudioFrame): Input audio frame.
+
+    Returns:
+        av.AudioFrame: Processed audio frame.
+    """
     recorder.start()
-   
     while True:
         pcm = recorder.read()
         is_voiced = cobra.process(pcm)
-        print(is_voiced)
+        if is_voiced:
+            print("Voice detecte!")
 
 
 with streaming_placeholder.container():
@@ -261,6 +278,6 @@ with streaming_placeholder.container():
         rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]},
         video_frame_callback=callback,
         audio_frame_callback=process_audio,
-        media_stream_constraints={"video": True, "audio": True},
+        media_stream_constraints={"video": True, "audio": False}, #
         async_processing=True,
     )
