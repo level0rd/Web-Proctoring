14828bb1d63411b52f2350a47ff11bd157506e06
level0rd <45522296+level0rd@users.noreply.github.com>
2024-04-17 01:58:38 +0300

    The class VideoFileWriter added.

--- web-proctoring/exam.py
+++ web-proctoring/exam.py
@@ -2,7 +2,8 @@ import datetime
 from enum import Enum
 import queue as Q
 import av
-from video_file_writer import VideoFileWriter
+import threading
+import cv2
 
 
 class Violation(Enum):
@@ -61,3 +62,58 @@ class Exam:
             self.should_stop_writing = False
             self.violation_fixed = False
             self.no_violation_counter = 0
+
+
+class VideoFileWriter:
+    def __init__(self, width: int, height: int, exam_state: Exam):
+        """
+        Initialize VideoFileWriter object.
+
+        Args:
+            width (int): Width of the video frames.
+            height (int): Height of the video frames.
+            exam_state (Exam): Exam object representing the current exam session.
+        """
+        self.width = width
+        self.height = height
+        self.exam_state = exam_state
+        self.fourcc = cv2.VideoWriter_fourcc(*'avc1')
+        self.out = None
+        self.file_writer_task = None
+        self.filename = None
+
+    def start_writing(self, filename: str):
+        """
+        Start writing frames to the video file in a separate thread.
+
+        Args:
+            filename (str): Name of the output video file.
+        """
+        self.filename = filename
+        self.out = cv2.VideoWriter(filename, self.fourcc, 24.0, (self.width, self.height))
+        self.file_writer_task = threading.Thread(target=self.write_frames_to_file)
+        self.file_writer_task.start()
+
+    def write_frames_to_file(self):
+        """
+        Write frames to the video file.
+        """
+        while not self.exam_state.should_stop_writing:
+            try:
+                frame = self.exam_state.frame_queue.get(timeout=1)
+                img_bgr = frame.to_ndarray(format="bgr24")
+                if self.out is None:
+                    self.start_writing(self.filename)
+                self.out.write(img_bgr)
+                self.exam_state.frame_queue.task_done()
+            except Q.Empty:
+                pass
+
+    def close(self):
+        """
+        Close the video file writer.
+        """
+        if self.out is not None:
+            self.out.release()
+        if self.file_writer_task is not None:
+            self.file_writer_task.join()
